47315c73128ebf2276e2b5206dc515aeb8c03cc2
shiftky <shiftky@gmail.com>
2015-01-27 16:23:14 +0900

    Fix install rbenv script

--- scripts/rbenv.sh
+++ scripts/rbenv.sh
@@ -3,11 +3,13 @@
 RBENV_REPO="https://github.com/sstephenson/rbenv.git"
 RUBY_BUILD_REPO="https://github.com/sstephenson/ruby-build.git"
 
-git clone $RBENV_REPO $HOME/.rbenv
-git clone $RUBY_BUILD_REPO $HOME/.rbenv/plugins/ruby-build
+if [ ! -e $HOME/.rbenv ]; then
+  git clone $RBENV_REPO $HOME/.rbenv
+  git clone $RUBY_BUILD_REPO $HOME/.rbenv/plugins/ruby-build
 
-echo
-echo 'Add rbenv settings to your shell settings.\n'
-echo '  export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"'
-echo '  eval "$(rbenv init -)"'
-echo
+  echo
+  echo 'Add rbenv settings to your shell settings.\n'
+  echo '  export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"'
+  echo '  eval "$(rbenv init -)"'
+  echo
+fi
