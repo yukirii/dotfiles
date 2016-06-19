677fa57f8da9ddf706685bcd018f4417b2f0cb8e
shiftky <shiftky@gmail.com>
2016-06-20 00:36:13 +0900

    Add install powerline script

---
+++ scripts/powerline.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+function install_python() {
+  brew list | grep python
+  ret=$?
+  if [ $ret != 0 ]; then
+    brew install python
+  fi
+}
+
+function install_powerline_status() {
+  pip list | grep powerline-status
+  ret=$?
+  if [ $ret != 0 ]; then
+    pip install --user powerline-status
+  fi
+}
+
+install_python || install_powerline_status
