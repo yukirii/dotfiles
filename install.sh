3e3e713f32fedaab6d488950108dbda42399086e
shiftky <shiftky@gmail.com>
2017-01-08 00:31:46 +0900

    Create entry point

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+MITAMAE_BIN="$HOME/bin/mitamae"
+MITAMAE_URL="https://github.com/k0kubun/mitamae/releases/download/v1.3.2/mitamae-i386-`uname`"
+
+# Install mitamae
+if [ ! -e $HOME/bin ]; then
+  mkdir $HOME/bin
+fi
+if [ ! -e $MITAMAE_BIN ]; then
+  wget $MITAMAE_URL -O $MITAMAE_BIN
+  chmod +x $MITAMAE_BIN
+fi
+$MITAMAE_BIN version
+
+# Run mitamae
+cd $HOME/.dotfiles
+$MITAMAE_BIN local -y nodes/`uname`.yml entry.rb
