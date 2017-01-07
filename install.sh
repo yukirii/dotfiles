e293362b14e75a3d8339597c5634045f78bf3189
shiftky <shiftky@gmail.com>
2017-01-08 01:44:29 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+DOTFILES_DIR="$HOME/.dotfiles"
 MITAMAE_BIN="$HOME/bin/mitamae"
 MITAMAE_URL="https://github.com/k0kubun/mitamae/releases/download/v1.3.2/mitamae-i386-`uname`"
 
-# Install homebrew
 if [ `uname` = 'Darwin' ]; then
+  # Install homebrew
   which brew > /dev/null
   if [ "$?" -ne 0 ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -21,6 +22,11 @@ if [ ! -e $MITAMAE_BIN ]; then
 fi
 $MITAMAE_BIN version
 
+# Clone shiftky/.dotfiles
+if [ ! -e $DOTFILES_DIR ]; then
+  git clone https://github.com/shiftky/dotfiles.git $DOTFILES_DIR
+fi
+cd $DOTFILES_DIR
+
 # Run mitamae
-cd $HOME/.dotfiles
 $MITAMAE_BIN local -y nodes/`uname`.yml entry.rb
