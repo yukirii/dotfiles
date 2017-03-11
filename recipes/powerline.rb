dcd7ceffdad510c93812698d2062c0ad60a809eb
shiftky <shiftky@gmail.com>
2017-03-11 17:01:31 +0900

    Add pyenv recipe

--- recipes/powerline.rb
+++ recipes/powerline.rb
@@ -1,4 +1,9 @@
+execute 'Install pip' do
+  command 'sudo easy_install pip'
+  not_if 'which pip'
+end
+
 execute 'Install powerline-status' do
-  command 'pip install powerline-status'
+  command 'pip install powerline-status --user'
   not_if 'pip list | grep powerline-status'
 end
---
+++ recipes/pyenv.rb
@@ -0,0 +1,3 @@
+git "#{ENV['HOME']}/.pyenv" do
+  repository node[:pyenv_repo]
+end
