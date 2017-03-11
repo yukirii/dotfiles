d2e758af1d0a20a020e91025939d5b89755e1b17
shiftky <shiftky@gmail.com>
2017-03-11 19:19:26 +0900

    Fix condition

--- recipes/iterm2-japanesque.rb
+++ recipes/iterm2-japanesque.rb
@@ -3,12 +3,12 @@ directory "#{ENV['HOME']}/.dotfiles/tmp" do
   action :create
 end
 
-execute "Install iterm2-japanesque" do
-  command <<-EOF
-    wget #{node[:iterm2_japanesque][:url]} -O #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors
-    if [ "$CI" != "true" ]; then
+if ENV['CONTINUOUS_INTEGRATION'] != 'true'
+  execute "Install iterm2-japanesque" do
+    command <<-EOF
+      wget #{node[:iterm2_japanesque][:url]} -O #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors
       open #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors
-    fi
-  EOF
-  not_if "test -e #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors"
+    EOF
+    not_if "test -e #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors"
+  end
 end
