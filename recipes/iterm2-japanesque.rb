16fcdb2cad21031f3732a20d8b89eccf6727d5e0
shiftky <shiftky@gmail.com>
2017-03-11 17:51:51 +0900

    Check iterm2 installed

--- recipes/iterm2-japanesque.rb
+++ recipes/iterm2-japanesque.rb
@@ -5,8 +5,10 @@ end
 
 execute "Install iterm2-japanesque" do
   command <<-EOF
-    wget #{node[:iterm2_japanesque][:url]} -O #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors
-    open #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors
-EOF
+    wget #{node[:iterm2_japanesque][:url]} -O #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors"
+    if test -e /Applications/iTerm.app; then
+      open "#{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors"
+    fi
+  EOF
   not_if "test -e #{ENV['HOME']}/.dotfiles/tmp/Japanesque.itermcolors"
 end
