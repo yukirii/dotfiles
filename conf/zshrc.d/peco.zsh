703b789f814f387ce2aa5d2e4163a9056367ccf0
Yuki Kirii <yukirii@lbread.org>
2020-06-06 17:59:35 +0900

    Add helper functions for kubectl using peco

--- conf/zshrc.d/peco.zsh
+++ conf/zshrc.d/peco.zsh
@@ -2,7 +2,7 @@ function peco-history-selection() {
   if [ `uname` = 'Darwin' ]; then
     BUFFER=`history -n 1 | tail -r | awk '!a[$0]++' | peco`
   else
-    BUFFER=`history -n 1 | tac | awk '!a[$0]++' | peco`
+    BUFFER=`history -n 1 | tac | awk '!a[$0]++' | peco | sed -e 's/\\n/\n/'`
   fi
   CURSOR=$#BUFFER
   zle reset-prompt
@@ -10,3 +10,21 @@ function peco-history-selection() {
 
 zle -N peco-history-selection
 bindkey '^R' peco-history-selection
+
+function peco-kubectl-switch-context(){
+  ctx=$(kubectl config get-contexts | peco --initial-index=1 --prompt='kubectl config use-context > ' | sed -e 's/^\*//' | awk '{ print $1 }')
+  if [ -n "$ctx" ]; then
+    kubectl config use-context $ctx
+  fi
+}
+
+alias kctx='peco-kubectl-switch-context'
+
+function peco-kubectl-ssh-jump(){
+  node=$(kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | peco --prompt='kubectl ssh-jump > ')
+  if [ -n "$node" ]; then
+    kubectl ssh-jump $node
+  fi
+}
+
+alias kssh='peco-kubectl-ssh-jump'
