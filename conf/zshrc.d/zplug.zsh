1e8fecf197432a1fa8b1a7d91ad2d1861e7f38d0
Yuki Kirii <1394098+yukirii@users.noreply.github.com>
2021-07-18 15:12:37 +0900

    Add zplug nnao45/zsh-kubectl-completion

--- conf/zshrc.d/zplug.zsh
+++ conf/zshrc.d/zplug.zsh
@@ -2,5 +2,6 @@ if [ -e $HOME/.zplug ]; then
   source $HOME/.zplug/init.zsh
   zplug "zsh-users/zsh-completions"
   zplug "zsh-users/zsh-syntax-highlighting", defer:2
+  zplug "nnao45/zsh-kubectl-completion"
   zplug load
 fi
