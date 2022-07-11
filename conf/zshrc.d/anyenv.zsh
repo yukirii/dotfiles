144cf10dc7cebfb57d5a4494ed2e32bcc2339146
Yuki Kirii <1394098+yukirii@users.noreply.github.com>
2022-07-11 12:41:03 +0900

    Cache anyenv
    * https://zenn.dev/ktakayama/articles/27b9d6218ed2f0ee9992

--- conf/zshrc.d/anyenv.zsh
+++ conf/zshrc.d/anyenv.zsh
@@ -2,5 +2,10 @@ if [ -e $HOME/.anyenv ]; then
   if [ -z $TMUX ]; then
     export PATH="$HOME/.anyenv/bin:$PATH"
   fi
-  eval "$(anyenv init - --no-rehash)"
+
+  if ! [ -f /tmp/anyenv.cache ]; then
+    anyenv init - --no-rehash > /tmp/anyenv.cache
+    zcompile /tmp/anyenv.cache
+  fi
+  source /tmp/anyenv.cache
 fi
