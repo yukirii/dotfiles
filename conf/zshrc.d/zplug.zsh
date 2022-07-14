42bb9dd79d60c75b31ac0cbc509a84fde63684ed
Yuki Kirii <1394098+yukirii@users.noreply.github.com>
2022-07-14 10:17:28 +0900

    lazy load

--- conf/zshrc.d/zplug.zsh
+++ conf/zshrc.d/zplug.zsh
@@ -1,6 +1,6 @@
 if [ -e $HOME/.zplug ]; then
   source $HOME/.zplug/init.zsh
-  zplug "zsh-users/zsh-completions"
-  zplug "zsh-users/zsh-syntax-highlighting", defer:2
+  zplug "zsh-users/zsh-completions", lazy:true
+  zplug "zsh-users/zsh-syntax-highlighting", defer:2, lazy:true
   zplug load
 fi
