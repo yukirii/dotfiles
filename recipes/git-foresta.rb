4185ac8ba326caeb29eaf855433e93d546e370ec
Yuki Kirii <1394098+yukirii@users.noreply.github.com>
2023-04-15 18:53:10 +0900

    Fix git-foresta recipe

--- recipes/git-foresta.rb
+++ recipes/git-foresta.rb
@@ -6,7 +6,7 @@ URL = "https://github.com/takaaki-kasai/git-foresta/raw/master/git-foresta"
 
 execute "Install git-foresta" do
   command <<-EOF
-    wget #{URL} -O #{ENV['HOME']}/bin/git-foresta
+    curl -L #{URL} -o #{ENV['HOME']}/bin/git-foresta
     chmod +x ~/bin/git-foresta
   EOF
   not_if "test -e #{ENV['HOME']}/bin/git-foresta"
